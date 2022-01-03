19043e3674f3d841e0e244090775e5c6a88a819e
Nadiia4 <nadushnichek@mail.ru>
2022-01-03 15:36:51 +0200

    print size

---
+++ src/test/java/firstProjectt/Test_1.java
@@ -0,0 +1,43 @@
+package firstProjectt;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Test_1 {
+
+    WebDriver wd;
+
+
+    @BeforeMethod
+    public void preConditions() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        wd = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Nadii/Qa31_Start2/chromedriver.exe");
+        wd.navigate().to("https://trello.com/");
+    }
+        @Test
+                public void testName1(){
+        // WebElement el = wd.findElement(By.tagName("a"));
+        WebElement el2 = wd.findElement(By.tagName("div"));
+        WebElement el3 = wd.findElement(By.className("text-primary"));
+        // WebElement link = wd.findElement(By.linkText("Sign Up"));
+        // WebElement partLink = wd.findElement(By.partialLinkText("Sign"));
+            List<WebElement> ael = wd.findElements(By.tagName("a"));
+            System.out.println(ael.size());
+    }
+    @AfterMethod
+    public void close(){
+        wd.close(); // vihod iz prilizeniya
+        wd.quit(); // vihod iz brauzera
+    }
+}
